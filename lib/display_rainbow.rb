9be410ffcdec811a1f0f36044d76a09671bad622
Amy Carney <amy.carney@alaska.gov>
2017-12-12 01:23:03 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,9 +3,9 @@ def display_rainbow(colors)
   colors = ['red','orange','yellow','green','blue','indigo','violet']
   puts "R: #{colors[0]}"
   puts "O: #{colors[1]}"
-  puts "Y: #{colors[3]}"
-  puts "G: #{colors[4]}"
-  puts "B: #{colors[5]}"
-  puts "I: #{colors[6]}"
-  puts "V: #{colors[7]}"
+  puts "Y: #{colors[2]}"
+  puts "G: #{colors[3]}"
+  puts "B: #{colors[4]}"
+  puts "I: #{colors[5]}"
+  puts "V: #{colors[6]}"
 end
